e84130728c5b2fa3586e98d73f981fa63dd15947
tretezztrzrt-svg <tretezztrzrt@gmail.com>
2026-07-04 07:22:41 +0000

    functions container proofed, aliases noch nicht

---
+++ .bash_aliases.sh
@@ -0,0 +1,2 @@
+# alias packen-tarfix='tar -czvf' # Komprimiert einen Ordner zu .tar.gz , vorherige kurzversion
+# alias packen-untar='tar -zxvf' # Entpackt eine .tar.gz Datei , vorherige kurzversion
