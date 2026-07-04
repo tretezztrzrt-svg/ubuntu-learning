138f9927b6429738d5ae92991a81af367abd170b
tretezztrzrt-svg <tretezztrzrt@gmail.com>
2026-07-04 16:40:14 +0200

    Update .bash_aliases.sh

--- my-bash/.bash_aliases.sh
+++ my-bash/.bash_aliases.sh
@@ -1,4 +1,10 @@
-# ---------------------------------------------------------
+# paar kurzepumpgun action --------------------------------
+alias c='clear' # Terminal leeren
+alias q='exit' # Beendet das Terminal mit einem Buchstaben
+alias h='history' # Zeigt die Befehlshistorie an
+alias x='chmod +x' # Macht eine Skriptdatei schnell ausführbar
+alias reboot='shutdown -r now' # System sofort neu starten
+# meins---------------------------------------------------
   alias alex='cd /home/alex/' # Wechselt in das Verzeichnis von Benutzer Alex
   alias home='cd /home/alex/' # Wechselt in das Home-Verzeichnis von Alex
   alias vera='cd /media/veracrypt1/' # Wechselt in den VeraCrypt-Mountpoint
@@ -13,29 +19,21 @@
   alias etc='cd /etc/' # Wechselt in das Systemkonfigurations-Verzeichnis
   
   alias install-common='sudo apt install cmatrix micro less mc btop p7zip-full brasero make git meld curl net-tools hwinfo gedit'
-  # try dis out sudo apt install cockpit
-  alias cockpit-install='sudo apt install cockpit'
-  
 # ---------------------------------------------------------
-# paar kurzepumpgun action --------------------------------
-alias c='clear' # Terminal leeren
-alias q='exit' # Beendet das Terminal mit einem Buchstaben
-alias h='history' # Zeigt die Befehlshistorie an
-alias x='chmod +x' # Macht eine Skriptdatei schnell ausführbar
-alias reboot='shutdown -r now' # System sofort neu starten
+# try dis out sudo apt install cockpit
+alias cockpit-install='sudo apt install cockpit'
+# help
+alias ao-help="echo 'man(pages) offline, https://tldr.inbrowser.app/, http://cheat.sh/'"
 
 # system -> scripts gewandert
 alias systemstatus='top -b -n 1 | head -n 20' # Schneller Überblick über die Top-Prozesse  # ubuntu-proofed
 alias uptime_p='uptime -p' # Zeigt an, wie lange das System bereits läuft  # ubuntu-proofed
-
 # ram
 alias ram='free -h --si' # Übersicht über belegten/freien RAM-Speicher (SI-Einheiten)
-
 # hdd platz
 alias platz='df -hPT | column -t' # Formatiert Festplattenplatz als saubere Tabelle
 alias platte='df -h' # Festplattenplatz in GB/MB anzeigen
 alias diskinfo='lsblk' # Listet Blockgeräte und Partitionen als Baum auf
-
 # paket-manager
 alias install='sudo apt install ' # Bestätigender Shortcut für Paketinstallation
 alias uninstall='sudo apt remove' # Deinstalliert ein Paket
@@ -49,32 +47,11 @@ alias killforce='kill -9' # Beendet einen Prozess sofort und unnachgiebig (SIGKI
 alias exorzist='kill -9' # Sendet das Standard-Beendigungssignal an eine PID (SIGTERM) -> Hinweis: Nutzt SIGKILL (-9)
 alias psaux='ps -aux' # Zeigt alle laufenden Prozesse im System an
 alias psax='ps -aux' # Zeigt alle laufenden Prozesse im System an
-
 # btop, btop, btop everything is btop!!!!!!
-# sudo apt install glances -> try it
 alias top='btop'
 alias btop='btop'
 alias htop='btop'
-
-# bisschen color
-alias grep='grep --color=auto' # Hebt grep-Suchtreffer farblich hervor
-alias egrep='egrep --color=auto' # Hebt egrep-Suchtreffer farblich hervor
-alias fgrep='fgrep --color=auto' # Hebt fgrep-Suchtreffer farblich hervor
-alias dir='dir --color=auto' # Aktiviert Farben für dir
-alias vdir='vdir --color=auto' # Aktiviert Farben für vdir
-alias l='ls -la --color=auto' # Standard-Spalten-Auflistung für schnelle Orientierung
-alias sl='ls -la --color=auto' # Korrigiert Dreher bei 'ls'
-alias ls='ls -la --color=auto' # Aktiviert Farben für ls
-alias sl="ls -la --color=auto"               # Vertippt von ls zu sl
-
-# Info-Modus-an
-alias cp='cp -i' # Interaktives Kopieren (Schutz vor Überschreiben)
-alias copy='cp -i' # Fragt vor dem Überschreiben beim Kopieren nach
-alias mv='mv -i' # Interaktives Verschieben (Schutz vor Überschreiben)
-alias move='mv -i' # Fragt vor dem Überschreiben beim Verschieben nach
-alias rm='rm -i' # Fragt vor jedem Löschen um Bestätigung
-alias remove='rm -i' # Fragt vor dem Löschen nach
-
+# sudo apt install glances -> try it
 # Misc -------------------------------------------------------------------
 alias warum='echo "Weil du der Admin bist. Atme tief durch."' # Trost bei Frust
 alias why='echo "Weil du der Admin bist. Atme tief durch und prüfe die Logs."' # Bereitstellung von Administrator-Trost
@@ -88,18 +65,29 @@ alias bitte="sudo \$(history -p !!)"
 # function please()
 alias pls="sudo \$(fc -ln -1)"
 # function fuck()
-
 # history
 alias hg="history | grep"
 alias history-grep="hg"
 # profile-me func() is fun
-
-# Wichtige Shortcuts
-alias ao-help_me="echo 'man'"
-alias tldr= 'echo "tldr.appinabrowsser"'
-alias cheatsh='echo "cheat.sh/cd"'
-
 # Uhrzeit & Datum
 alias date_now="date '+%Y-%m-%d %H:%M:%S'"
 # Schleifen Sammlung
 alias schleife='echo "while true; do clear; ls -la; sleep 1; done"' # macht es so lange bis unendlich
+
+# bisschen color
+alias grep='grep --color=auto' # Hebt grep-Suchtreffer farblich hervor
+alias egrep='egrep --color=auto' # Hebt egrep-Suchtreffer farblich hervor
+alias fgrep='fgrep --color=auto' # Hebt fgrep-Suchtreffer farblich hervor
+alias dir='dir --color=auto' # Aktiviert Farben für dir
+alias vdir='vdir --color=auto' # Aktiviert Farben für vdir
+alias l='ls -la --color=auto' # Standard-Spalten-Auflistung für schnelle Orientierung
+alias sl='ls -la --color=auto' # Korrigiert Dreher bei 'ls'
+alias ls='ls -la --color=auto' # Aktiviert Farben für ls
+alias sl="ls -la --color=auto"               # Vertippt von ls zu sl
+# Info-Modus-an
+alias cp='cp -i' # Interaktives Kopieren (Schutz vor Überschreiben)
+alias copy='cp -i' # Fragt vor dem Überschreiben beim Kopieren nach
+alias mv='mv -i' # Interaktives Verschieben (Schutz vor Überschreiben)
+alias move='mv -i' # Fragt vor dem Überschreiben beim Verschieben nach
+alias rm='rm -i' # Fragt vor jedem Löschen um Bestätigung
+alias remove='rm -i' # Fragt vor dem Löschen nach
